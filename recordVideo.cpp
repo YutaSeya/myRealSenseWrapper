6d15df7c0933edc1db09bb4d8ac1bea2aafd0cbb
YutaSeya <bp17069@shibaura-it.ac.jp>
2020-02-15 13:36:54 +0900

    add record class

--- recordVideo.cpp
+++ recordVideo.cpp
@@ -1,1 +1,22 @@
 #include "recordVideo.h"
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+recordVideo::recordVideo()
+{
+
+}
+
+void recordVideo::init(std::string filename, int _fps, cv::Size _ksize)
+{
+	fps = _fps;
+	// * エンコード形式 "XVID" = AVI, "MP4V" = MPEG4, "WMV1" = WMV
+	fourcc = cv::VideoWriter::fourcc('X', 'V', 'I', 'D');
+	writer.open(filename, fourcc, _fps, _ksize);
+}
+
+void recordVideo::record(cv::Mat _frame)
+{
+	writer.write(_frame);
+}
--- recordVideo.h
+++ recordVideo.h
@@ -1,6 +1,20 @@
 #pragma once
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
 class recordVideo
 {
+private:
+	int fourcc = 0;
+	int fps = 30;
+	cv::VideoWriter writer;
 
+public:
+	recordVideo();
+
+	void init(std::string filename, int _fps, cv::Size _ksize);
+
+	void record(cv::Mat _frame);
 };
 
